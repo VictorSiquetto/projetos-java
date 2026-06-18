a28b270ed257be98db646e288d3368dba0211640
Victor Siquetto <victor.siquetto@gmail.com>
2026-06-18 01:39:41 -0300

    exercicio finalizado

---
+++ exercicios-java-poo/Projeto02/src/projeto01/Caneta.java
@@ -0,0 +1,35 @@
+
+package projeto01;
+
+
+public class Caneta {
+    public String modelo;
+    public String cor;
+    private float ponta;
+    protected int carga;
+    private boolean tampada;
+    
+    public void status(){
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga: " + this.carga);
+        System.out.println("Esta tampada? " + this.tampada);
+    }
+    
+    public void tampar(){
+        this.tampada = true;
+    }
+    
+    public void destampar(){
+        this.tampada = false;
+    }
+    
+    public void rabiscar(){
+        if (this.tampada){
+            System.out.println("ERRO a caneta esta tampada");
+        }else{
+            System.out.println("Estou rabiscando");
+        }
+    }
+}
