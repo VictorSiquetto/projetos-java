e8a359151dc1e83a484501889d206a5e6e6d4e18
Victor Siquetto <victor.siquetto@gmail.com>
2026-08-17 01:25:46 -0300

    exercicio finalizado

---
+++ exercicios-java-poo/Projeto05/src/projeto05/Projeto05.java
@@ -0,0 +1,14 @@
+package projeto05;
+
+public class Projeto05 {
+
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.play();
+        c.maisVolume();
+        c.abrirMenu();
+        c.fecharMenu();
+    }
+    
+}
